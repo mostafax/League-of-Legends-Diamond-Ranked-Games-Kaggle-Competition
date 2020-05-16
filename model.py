73f6059790a930ed7eed857574fe872b9c979a62
mostafax <mostafamousa1080p@gmail.com>
2020-05-16 21:05:32 +0200

    Adding model

---
+++ model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import  numpy as np
+import  matplotlib.pyplot as plt
+from sklearn.model_selection import  train_test_split
+import keras
+from keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+from keras.utils import to_categorical
+
+data_set = pd.read_csv('high_diamond_ranked_10min.csv')
+
+
+
+std = StandardScaler()
+std.fit(data_set)
+data_set = pd.DataFrame(std.transform(data_set),columns=data_set.columns)
+
+target=data_set['blueWins']
+target=to_categorical(target, 2)
+
+features = data_set.iloc[:,2:].values
+
+x_train,x_test,y_train,y_test = train_test_split(features,target,test_size=0.2, random_state=42,shuffle=True)
+
+model = keras.Sequential()
+model.add(Dense(20,activation='relu',kernel_initializer='uniform',input_dim=38))
+
+model.add((Dense(2,activation='sigmoid',kernel_initializer='uniform')))
+
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),loss='binary_crossentropy', metrics=['accuracy',])
+model.fit(x_train, y_train, batch_size=32, epochs=80,validation_data=(x_test, y_test))
+predictions = model.predict(x_test)
+print(predictions)
+
+
+model.evaluate(x_test, y_test)
