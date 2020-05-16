4c57d60ac004d56428344d7655ea12617d2f12d0
mostafax <mostafamousa1080p@gmail.com>
2020-05-16 21:24:41 +0200

    Adding Readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # League-of-Legends-Diamond-Ranked-Games-Kaggle-Competition
+
+## Getting Started
+
+League of Legends is a MOBA (multiplayer online battle arena) where 2 teams (blue and red) face off. There are 3 lanes, a jungle, and 5 roles. The goal is to take down the enemy Nexus to win the game.
+
+The column blueWins is the target value (the value we are trying to predict). A value of 1 means the blue team has won. 0 otherwise. 
+
+
+### Installing
+
+This model runs on Python 3.7.0 bulit with Keras
+
+Install the Required libraries
+
+```
+pip install -r requirements.txt
+```
+
+### Run The model
+
+```
+python model.py
+```
+
+### Evaltation
+
+The model scored 72% on the test set after training for 100 epoch.
--- model.py
+++ model.py
@@ -7,30 +7,30 @@ from keras.layers import Dense
 from sklearn.preprocessing import StandardScaler
 from keras.utils import to_categorical
 
+#data Loading
 data_set = pd.read_csv('high_diamond_ranked_10min.csv')
 
 
-
+#data Normalization
 std = StandardScaler()
 std.fit(data_set)
 data_set = pd.DataFrame(std.transform(data_set),columns=data_set.columns)
 
+#data split
 target=data_set['blueWins']
 target=to_categorical(target, 2)
-
 features = data_set.iloc[:,2:].values
-
 x_train,x_test,y_train,y_test = train_test_split(features,target,test_size=0.2, random_state=42,shuffle=True)
 
+#model define
 model = keras.Sequential()
 model.add(Dense(20,activation='relu',kernel_initializer='uniform',input_dim=38))
-
 model.add((Dense(2,activation='sigmoid',kernel_initializer='uniform')))
 
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),loss='binary_crossentropy', metrics=['accuracy',])
+#model train
 model.fit(x_train, y_train, batch_size=32, epochs=80,validation_data=(x_test, y_test))
 predictions = model.predict(x_test)
-print(predictions)
 
-
-model.evaluate(x_test, y_test)
+#model test
+print(model.evaluate(x_test, y_test))
